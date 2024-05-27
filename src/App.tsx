1d73722b2f510a6f23fed649a84aa750a9d951e3
Brian Snack <snackbrian10@gmail.com>
2024-05-27 10:34:46 -0300

    FRONT - Consultar CEP

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ProdutoListar from "./components/pages/produto/produto-listar";
+function App() {
+  return (
+    <ProdutoListar></ProdutoListar>
+  );
+}
+
+export default App;
